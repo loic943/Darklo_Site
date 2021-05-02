b65bc1a4653c7b979371b8563cd73b2d86c766d9
KERDANET Loic <loic943@gmail.com>
2021-05-02 21:11:58 +0200

    HomeTest non fonctionnel

---
+++ tests/HomeTest.php
@@ -0,0 +1,17 @@
+<?php
+/*
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomeTest extends WebTestCase
+{
+    public function testHomePage(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        //$this->assertSelectorTextContains('h1', 'Bienvenue');
+    }
+} */
